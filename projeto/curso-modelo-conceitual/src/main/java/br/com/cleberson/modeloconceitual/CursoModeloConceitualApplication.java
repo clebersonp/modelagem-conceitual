7d8f23b07292eb85a921073298e2abaa1844267d
cleberson <pauluci.cleberson@gmail.com>
2018-01-30 23:21:15 -0200

    commit inicial - projeto criado

---
+++ projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/CursoModeloConceitualApplication.java
@@ -0,0 +1,12 @@
+package br.com.cleberson.modeloconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoModeloConceitualApplication.class, args);
+	}
+}
