b975aca458d59d19773a06322ae324a2927fa54e
cleberson <pauluci.cleberson@gmail.com>
2018-01-31 01:11:09 -0200

    criando operacao de instanciacao de dados no dao

--- projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/CursoModeloConceitualApplication.java
+++ projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/CursoModeloConceitualApplication.java
@@ -1,12 +1,31 @@
 package br.com.cleberson.modeloconceitual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoModeloConceitualApplication {
+import br.com.cleberson.modeloconceitual.domain.Categoria;
+import br.com.cleberson.modeloconceitual.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoModeloConceitualApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModeloConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Limpeza");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3));
 	}
 }
