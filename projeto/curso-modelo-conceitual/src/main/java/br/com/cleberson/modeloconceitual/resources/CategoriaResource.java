b7b57b8f6d970414a9b8c0e14a3e0a2794c861a8
cleberson <pauluci.cleberson@gmail.com>
2018-01-31 01:05:26 -0200

    refatorado metodos de resource

--- projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/resources/CategoriaResource.java
+++ projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/resources/CategoriaResource.java
@@ -1,27 +1,31 @@
 package br.com.cleberson.modeloconceitual.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cleberson.modeloconceitual.domain.Categoria;
+import br.com.cleberson.modeloconceitual.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+	@Autowired
+	private CategoriaService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1L, "Informática");
-		Categoria cat2 = new Categoria(2L, "Escritório");
-		
-		List<Categoria> categorias = Arrays.asList(cat1, cat2);
-		
-		return categorias;
+	public ResponseEntity<List<Categoria>> listar() {
+		return ResponseEntity.ok(service.listar());
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> buscar(@PathVariable Long id) {
+		return ResponseEntity.ok(service.buscar(id));
+	}
 }
