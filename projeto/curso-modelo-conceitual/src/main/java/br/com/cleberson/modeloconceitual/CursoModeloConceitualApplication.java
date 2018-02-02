1f322aae87fd38654570af2746b67c65d7475710
cleberson <pauluci.cleberson@gmail.com>
2018-02-02 01:11:00 -0200

    produto, categoria e associacao many to many

--- projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/CursoModeloConceitualApplication.java
+++ projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/CursoModeloConceitualApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.cleberson.modeloconceitual.domain.Categoria;
+import br.com.cleberson.modeloconceitual.domain.Produto;
 import br.com.cleberson.modeloconceitual.repositories.CategoriaRepository;
+import br.com.cleberson.modeloconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoModeloConceitualApplication implements CommandLineRunner {
 
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModeloConceitualApplication.class, args);
@@ -24,8 +29,26 @@ public class CursoModeloConceitualApplication implements CommandLineRunner {
 	public void run(String... arg0) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Limpeza");
 		
-		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3));
+		Produto prod1 = new Produto(null, "Computador", 2000.0);
+		Produto prod2 = new Produto(null, "Impressora", 800.0);
+		Produto prod3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.add(prod1);
+		cat1.add(prod2);
+		cat1.add(prod3);
+		
+		cat2.add(prod2);
+		
+		prod1.add(cat1);
+		
+		prod2.add(cat1);
+		prod2.add(cat2);
+		
+		prod3.add(cat1);
+		
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(prod1, prod2, prod3));
 	}
 }
--- projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/domain/Produto.java
+++ projeto/curso-modelo-conceitual/src/main/java/br/com/cleberson/modeloconceitual/domain/Produto.java
@@ -8,27 +8,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
-	private static final long serialVersionUID = -3381521579861758966L;
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	private Double preco;
+
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+			joinColumns = @JoinColumn(name = "produto_id"),
+			inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
-
-	public Categoria() {
+	public Produto() {
 	}
-
-	public Categoria(Long id, String nome) {
+	
+	public Produto(Long id, String nome, Double preco) {
+		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Long getId() {
@@ -37,7 +46,7 @@ public class Categoria implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -45,20 +54,28 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 	
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 	
-	public void add(Produto produto) {
-		this.produtos.add(produto);
+	public void add(Categoria categoria) {
+		this.categorias.add(categoria);
 	}
 
 	@Override
 	public String toString() {
-		return "Categoria [getId()=" + getId() + ", getNome()=" + getNome() + ", getProdutos()=" + getProdutos() + "]";
+		return "Produto [getId()=" + getId() + ", getNome()=" + getNome() + ", getPreco()=" + getPreco()
+				+ ", getCategorias()=" + getCategorias() + "]";
 	}
 
 	@Override
@@ -77,7 +94,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
